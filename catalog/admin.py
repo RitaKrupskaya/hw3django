83c675130281b694fb3183cf0ee1405a934c10cc
Margarita Krupskaya <ritakrupskaya@bk.ru>
2024-07-08 18:03:56 +0300

    delete 0002

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 
-class CatalogAdmin(admin.ModelAdmin):
-    pass
+from catalog.models import Product, Category
+
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price_for_buy', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
