69ca7748e5e34fec875b9a9b2c5961d193814bbe
Margarita Krupskaya <ritakrupskaya@bk.ru>
2024-08-10 21:09:17 +0300

    ДЗ Аутентификация. Задание 2, пункт 2. Добавила верификацию почты пользователя через отправленное письмо.

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,11 +5,9 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        user = User.objects.create(email='admin@example.com')
-        user.set_password('admin123')
+        user = User.objects.create(email="admin@example.com")
+        user.set_password("admin123")
         user.is_active = True
         user.is_staff = True
         user.is_superuser = True
         user.save()
-
-
